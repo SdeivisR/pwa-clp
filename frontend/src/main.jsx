a27be0dec0b91cf5f8140c42d060c84c8d1e33b5
SdeivisR <sdeivisr@gmail.com>
2025-08-13 12:02:20 -0500

    avanze y funcionamiento de tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 👈 Aquí cargamos Tailwind
-import './App.css' 
+import './index.css'
+import './tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
